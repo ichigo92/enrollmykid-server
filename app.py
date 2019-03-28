a147060ca510845c7aaf6d2f597f44ad872404cf
Abdul Fatir <a.fatir@yahoo.com>
2019-03-28 09:06:50 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from cassandra.cluster import Cluster
 import requests
 import sys
 
-cluster = Cluster(['cassandra'])
+#cluster = Cluster(['cassandra'])
+cluster = Cluster(['127.0.0.1'])
 session = cluster.connect()
 
 app = Flask(__name__)
@@ -53,8 +54,7 @@ def create_centre():
 @app.route('/centres/<int:id>', methods = ['PUT'])
 def update_ccentre(id):
 
-    rows = session.execute("""UPDATE enrollmykid.centres SET email=%(email)s WHERE id=%(id)s""", {'email': request.form['email'], 'id': id})
-    print(rows,file=sys.stderr)
+    rows = session.execute("""UPDATE enrollmykid.centres SET serviceapprovalnumber=%(serviceapprovalnumber)s, providerapprovalnumber=%(providerapprovalnumber)s, servicename=%(servicename)s, providerlegalname=%(providerlegalname)s, serviceaddress=%(serviceaddress)s, suburb=%(suburb)s, state=%(state)s, postcode=%(postcode)s, phone=%(phone)s, fax=%(fax)s, email=%(email)s, conditionsonapproval=%(conditionsonapproval)s, numberofapprovedplaces=%(numberofapprovedplaces)s, overallrating=%(overallrating)s, type=%(type)s  WHERE id=%(id)s""", { 'serviceapprovalnumber': request.form['serviceApprovalNumber'], 'providerapprovalnumber' : request.form['providerApprovalNumber'], 'servicename': request.form['serviceName'], 'providerlegalname' : request.form['providerLegalName'], 'serviceaddress' : request.form['serviceAddress'], 'suburb' : request.form['suburb'], 'state':request.form['state'], 'postcode' : int(request.form['postcode']), 'phone':request.form['phone'], 'fax' : request.form['Fax'], 'email' : request.form['email'], 'conditionsonapproval':request.form['conditionsOnApproval'], 'numberofapprovedplaces' : int(request.form['numberOfApprovedPlaces']), 'overallrating':request.form['overallRating'], 'type':request.form['type'], 'id':id})
 
     return jsonify({'message':'updated successfully'})
 
@@ -62,7 +62,7 @@ def update_ccentre(id):
 @app.route('/centres/<int:id>', methods = ['DELETE'])
 def delete_centre(id):
 
-    session.execute("DELETE FROM enrollmykid.centres WHERE id=(%s)", (id,))
+    session.execute("""DELETE FROM enrollmykid.centres WHERE id={}""".format(id))
     return jsonify({'message':'delete successfull'})
 
 if __name__ == '__main__':
